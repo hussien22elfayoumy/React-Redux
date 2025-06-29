f3e68ce3547ba05c2214212b60672dd858087deb
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-06-29 19:45:49 +0300

    payloads, multi state propperties

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,23 +3,32 @@ import classes from './counter.module.css';
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
+
+  const increaseByValue = (payload) => {
+    dispatch({ type: 'increase', payload });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={() => increaseByValue(5)}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,31 @@
 import { legacy_createStore as createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
+      ...state,
       counter: state.counter + 1,
     };
   }
+
+  if (action.type === 'increase') {
+    return {
+      ...state,
+      counter: state.counter + action.payload,
+    };
+  }
+
   if (action.type === 'decrement') {
-    return { counter: state.counter - 1 };
+    return { ...state, counter: state.counter - 1 };
+  }
+
+  if (action.type === 'toggle') {
+    return {
+      ...state,
+      showCounter: !state.showCounter,
+    };
   }
 
   return state;
